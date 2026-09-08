8d3f709f5489e757d84580d4c1b4407e3eafb0ae
Manolo Müller <manolo.mueller@gmail.com>
2026-09-08 14:51:28 +0200

    made filter coefficient calculation non-allocating

--- source/Module/Filter/Filter.h
+++ source/Module/Filter/Filter.h
@@ -1,6 +1,7 @@
 #ifndef MODULE_FILTER_H
 #define MODULE_FILTER_H
 
+#include <atomic>
 #include <vector>
 #include <Module.h>
 #include <FilterConfig.h>
@@ -15,6 +16,8 @@ class Filter : public Module {
         size_t initialize(size_t input_channels) override;
         void prepare(HostAudioConfig host_audio_config) override;
         void process(AudioBufferF &buffer, size_t n_frames) override;
+
+        void set_enabled(bool is_enabled_);
     private:
         FilterConfigPtr m_config;
         void calculate_filter_coefficients(double samplerate);
@@ -24,7 +27,13 @@ class Filter : public Module {
         std::vector<std::vector<double>> m_memory_2;
         
         double filter_sample(double new_value, std::vector<double> &memory);
+        static int osc_filter_frequency_callback(const char *path, const char *types, lo_arg **argv, int argc, lo_message data, void *user_data);
         int m_n_taps;
+        std::atomic<bool> is_enabled {true};
+
+        // filter coefficient internal state stuff
+        std::vector<int> m_b_coeffs_int;
+        std::vector<double> m_rcof;
 };
 
 #endif //MODULE_FILTER_H
