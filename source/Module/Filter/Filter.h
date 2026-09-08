14970b475b2281000f10086f2297788ebaea3441
Manolo Müller <manolo.mueller@gmail.com>
2026-09-08 15:30:42 +0200

    factored out coefficient calculator

--- source/Module/Filter/Filter.h
+++ source/Module/Filter/Filter.h
@@ -20,7 +20,7 @@ class Filter : public Module {
         void set_enabled(bool is_enabled_);
     private:
         FilterConfigPtr m_config;
-        void calculate_filter_coefficients(double samplerate);
+        void calculate_filter_coefficients();
         std::vector<double> m_a;
         std::vector<double> m_b;
         std::vector<std::vector<double>> m_memory_1;
@@ -30,10 +30,7 @@ class Filter : public Module {
         static int osc_filter_frequency_callback(const char *path, const char *types, lo_arg **argv, int argc, lo_message data, void *user_data);
         int m_n_taps;
         std::atomic<bool> is_enabled {true};
-
-        // filter coefficient internal state stuff
-        std::vector<int> m_b_coeffs_int;
-        std::vector<double> m_rcof;
+        filter_coeff_calculator coeff_calc;
 };
 
 #endif //MODULE_FILTER_H
